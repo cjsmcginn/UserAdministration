a05c384fbec91ef435439e6bece83d0702a5e1af
Chris McGinn <chris.s.mcginn@live.com>
2013-10-25 07:55:15 -0400

    Added test for pager directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,27 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+angular.module('myApp.directives', []).directive('pager', function ($compile) {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: false,
+        render: function (scope, element, attrs) {
+            if(!scope.pager)
+                return;
+            var tp = '<ul class="pager">';
+            for (var i = 0; i < scope.pager.totalPages; i++)
+                tp += '<li><a href="javascript:void(0)" ng-click="page('+(i + 1)+')">' + (i + 1) + '</a></li>'
+            tp += '</ul>';
+            element.html(tp);
+            $compile(element.contents())(scope);
+        },
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.ngModel, function () {
+                if(scope.pager)
+                    directiveDefinitionObject.render(scope,element,attrs);
+            });
+        }
     };
-  }]);
+    return directiveDefinitionObject;
+});
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,20 +2,30 @@
 
 /* jasmine specs for directives go here */
 
-/*
-describe('directives', function() {
-  beforeEach(module('myApp.directives'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+describe('directives', function () {
+    var $scope, $compile;
+    beforeEach(module('myApp.directives'));
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+        $scope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+    describe('pager',function(){
+       it('should render list when pager model changes',function(){
+            $scope.pager = {};
+           var element = $compile('<pager ng-model="pager"></pager>')($scope);
+           expect(element.html()).not.toMatch(/li/);
+           $scope.pager ={
+               list:[1,2,3,4],
+                   pageIndex:1,
+               totalRecords:4,
+               pageSize:1,
+               totalPages:4
+           };
+           $scope.$digest();
+           expect(element.html()).toMatch(/li/);
+       });
     });
-  });
+
 });
-*/
+
